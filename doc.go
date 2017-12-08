b3db6bc5bcd7efc268df6aab90a0ffd13d3a24e0
Michael Kenney <mkenney@webbedlam.com>
2017-12-07 21:37:04 -0700

    docs

--- doc.go
+++ doc.go
@@ -24,17 +24,20 @@ guaranteed for the capabilities it introduces.
 
 Resources
 
- * chrome-debugging-protocol mailing list https://groups.google.com/d/forum/chrome-debugging-protocol
- * devtools-protocol repo issue tracker https://github.com/chromedevtools/devtools-protocol
- * Getting Started with Headless Chrome https://developers.google.com/web/updates/2017/04/headless-chrome
- * Headless Chromium readme https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+	* chrome-debugging-protocol mailing list https://groups.google.com/d/forum/chrome-debugging-protocol
+	* devtools-protocol repo issue tracker https://github.com/chromedevtools/devtools-protocol
+		can also be used for concerns with the protocol. It also hosts the canonical copy of the
+		json files.
+	* Getting Started with Headless Chrome https://developers.google.com/web/updates/2017/04/headless-chrome
+	* Headless Chromium readme https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+	* chrome-remote-interface node module https://github.com/cyrus-and/chrome-remote-interface/
+		Also the wiki https://github.com/cyrus-and/chrome-remote-interface/wiki
+		and issue tracker https://github.com/cyrus-and/chrome-remote-interface/issues?utf8=%E2%9C%93&q=is%3Aissue%20
+	*  awesome-chrome-devtools page https://github.com/ChromeDevTools/awesome-chrome-devtools#chrome-devtools-protocol
+		links to many of the tools in the protocol ecosystem, including protocol API libraries in
+		JavaScript, TypeScript, Python, Java, and Go. Many applications and libraries already use
+		the protocol.
 
-The chrome-remote-interface node module is recommended, and its wiki and issue tracker are full of
-useful recipes.
-
-The awesome-chrome-devtools page links to many of the tools in the protocol ecosystem, including
-protocol API libraries in JavaScript, TypeScript, Python, Java, and Go. Many applications and
-libraries already use the protocol.
 
 Basics: Using DevTools as protocol client
 
@@ -42,22 +45,22 @@ The Developer Tools front-end can attach to a remotely running Chrome instance f
 this scenario to work, you should start your host Chrome instance with the remote-debugging-port
 command line switch:
 
-chrome.exe --remote-debugging-port=9222
+	chrome.exe --remote-debugging-port=9222
+
 Then you can start a separate client Chrome instance, using a distinct user profile:
 
-chrome.exe --user-data-dir=<some directory>
+	chrome.exe --user-data-dir=<some directory>
+
 Now you can navigate to the given port from your client and attach to any of the discovered tabs for
 debugging: http://localhost:9222
 
 You will find the Developer Tools interface identical to the embedded one and here is why:
 
-* When you navigate your client browser to the remote's Chrome port, Developer Tools front-end is
-being served from the host Chrome as a Web Application from the Web Server.
-
-* It fetches HTML, JavaScript and CSS files over HTTP
-
-* Once loaded, Developer Tools establishes a Web Socket connection to its host and starts exchanging
-JSON messages with it.
+	* When you navigate your client browser to the remote's Chrome port, Developer Tools front-end
+	  is being served from the host Chrome as a Web Application from the Web Server.
+	* It fetches HTML, JavaScript and CSS files over HTTP
+	* Once loaded, Developer Tools establishes a Web Socket connection to its host and starts
+	  exchanging JSON messages with it.
 
 In this scenario, you can substitute Developer Tools front-end with your own implementation. Instead
 of navigating to the HTML page at http://localhost:9222, your application can discover available
@@ -74,24 +77,23 @@ Sniffing the protocol
 This is especially handy to understand how the DevTools frontend makes use of the protocol. First,
 run Chrome with the debugging port open:
 
-alias canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
-canary --remote-debugging-port=9222 http://localhost:9222 http://chromium.org
+	alias canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary" canary --remote-debugging-port=9222 http://localhost:9222 http://chromium.org
 
 Then, select the Chromium Projects item in the Inspectable Pages list. Now that DevTools is up and
 fullscreen, open DevTools to inspect it. Cmd-R in the new inspector to make the first restart. Now
 head to Network Panel, filter by Websocket, select the connection and click the Frames tab. Now you
 can easily see the frames of WebSocket activity as you use the first instance of the DevTools.
 
-Sniffing the debugger protocol
+DevTools protocol via Chrome extension
 
 To allow chrome extensions to interact with the protocol, we introduced chrome.debugger extension
 API that exposes this JSON message transport interface. As a result, you can not only attach to the
 remotely running Chrome instance, but also instrument it from its own extension.
 
 Chrome Debugger Extension API provides a higher level API where command domain, name and body are
-provided explicitly in the sendCommand call. This API hides request ids and handles binding of the
-request with its response, hence allowing sendCommand to report result in the callback function
-call. One can also use this API in combination with the other Extension APIs.
+provided explicitly in the `sendCommand`` call. This API hides request ids and handles binding of
+the request with its response, hence allowing `sendCommand`` to report result in the callback
+function call. One can also use this API in combination with the other Extension APIs.
 
 If you are developing a Web-based IDE, you should implement an extension that exposes debugging
 capabilities to your page and your IDE will be able to open pages with the target application, set
@@ -99,10 +101,11 @@ breakpoints there, evaluate expressions in console, live edit JavaScript and CSS
 network interaction and any other aspect that Developer Tools is instrumenting today.
 
 Opening embedded Developer Tools will terminate the remote connection and thus detach the extension.
+https://chromedevtools.github.io/devtools-protocol/#simultaneous
 
 Frequently Asked Questions
 
-How is the protocol defined?
+How is the protocol defined
 
 The canonical protocol definitions live in the Chromium source tree: (browser_protocol.json and
 js_protocol.json). They are maintained manually by the DevTools engineering team. These files are
@@ -112,28 +115,28 @@ The declarative protocol definitions are used across tools. Within Chromium, a b
 created for the Chrome DevTools to interact with, and separately the protocol is used for Chrome
 Headless’s C++ interface.
 
-What’s the protocol_externs file?
+What’s the protocol_externs file
 
 It’s created via generate_protocol_externs.py and useful for tools using closure compiler. The
 TypeScript story is here.
 
-Are the HTTP endpoints documented?
+Are the HTTP endpoints documented
 
 Not yet. See bugger-daemon’s third-party docs. See also the endpoints implementation in Chromium.
 /json/protocol was added in Chrome 60.
 
-How do I access the browser target?
+How do I access the browser target
 
 The endpoint is exposed as webSocketDebuggerUrl in /json/version. Note the browser in the URL,
 rather than page. If Chrome was launched with --remote-debugging-port=0 and chose an open port, the browser endpoint is written to both stderr and the DevToolsActivePort file in browser profile folder.
 
-Does the protocol support multiple simultaneous clients?
+Does the protocol support multiple simultaneous clients
 
 Yes, as of Chrome 63! See Multi-client remote debugging support.
 
 Upon disconnnection, the outgoing client will receive a detached event. For example:
 
-{"method":"Inspector.detached","params":{"reason":"replaced_with_devtools"}}.
+	{"method":"Inspector.detached","params":{"reason":"replaced_with_devtools"}}.
 
 View the enum of possible reasons. (For reference: the original patch). After disconnection, some
 apps have chosen to pause their state and offer a reconnect button.
