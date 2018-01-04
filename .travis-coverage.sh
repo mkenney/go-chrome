13662c1690ebe0ae8ad12f8a2b22abbe726e6697
Michael Kenney <mkenney@webbedlam.com>
2018-01-03 19:44:59 -0700

    code coverage

--- .travis-coverage.sh
+++ .travis-coverage.sh
@@ -4,9 +4,13 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     go test -coverprofile=profile.out $d
+    exit_code=$?
+    if [ 0 -ne $exit_code ]; then
+        exit $exit_code
+    fi
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
-
+exit 0
