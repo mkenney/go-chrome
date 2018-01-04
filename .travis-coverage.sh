24ca0cc5610439a17cdb44676b335505a5854236
Michael Kenney <mkenney@webbedlam.com>
2018-01-03 19:16:34 -0700

    code coverage

--- .travis-coverage.sh
+++ .travis-coverage.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
