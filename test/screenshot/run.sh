835b0b25f0f63fb1c1d3c7c71ecbe2364f68c395
Michael Kenney <mkenney@webbedlam.com>
2018-04-16 14:53:39 -0600

    path fix

--- test/screenshot/run.sh
+++ test/screenshot/run.sh
@@ -2,7 +2,7 @@
 docker run -it --rm \
 	-v $(pwd):/go/src/test \
 	-v $(pwd):/tmp/test \
-	-v $(pwd)../:/go/src/github.com/mkenney/go-chrome:ro \
+	-v $(pwd)/../:/go/src/github.com/mkenney/go-chrome:ro \
 	-e LOG_LEVEL=info \
 	mkenney/chromium-headless:latest \
 		sh -cexu "cd /go/src/test \
