5797fd534365b85abfa5e48689e58a5a9cc6ecc4
Michael Kenney <michael.kenney@returnpath.com>
2018-05-21 21:24:26 -0600

    revert vendor folder

--- test/dom/vendor/github.com/mkenney/go-chrome/tot/cdtp/device/orientation/cdtp.go
+++
@@ -1,7 +0,0 @@
-/*
-Package orientation provides type definitions for use with the Chrome DeviceOrientation
-protocol
-
-https://chromedevtools.github.io/devtools-protocol/tot/DeviceOrientation/
-*/
-package orientation
