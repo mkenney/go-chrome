ec14dc114e093f53b00f72fc87b831734e36c93b
Michael Kenney <mkenney@webbedlam.com>
2018-09-15 11:57:30 -0600

    ci updates

--- .travis.sh
+++ .travis.sh
@@ -26,7 +26,7 @@ done
 
 rm -f coverage.txt
 for dir in $(go list ./... | grep -v vendor); do
-    GOCACHE=off go test -race -timeout 300s -coverprofile=profile.out $dir
+    GOCACHE=off go test -timeout 300s -coverprofile=profile.out $dir
     exit_code=$?
     if [ "0" != "$exit_code" ]; then
         exit $exit_code
